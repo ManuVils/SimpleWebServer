e9ef104d0b43e7eec84505e9b4fbd58e41deaf11
Emanuel Rodrigues <emanueldavid95@hotmail.com>
2024-01-22 12:03:40 +0000

    Created a new directory resources and added the files the server will send back to the client. Instantiated each file on the ServerOptions class and created each respective header.

--- src/org/codeforall/ooptimus/Server.java
+++ src/org/codeforall/ooptimus/Server.java
@@ -1,9 +1,6 @@
 package org.codeforall.ooptimus;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -22,7 +19,10 @@ public class Server {
                 String clientRequest = requestReader.split(" ")[1];
 
                 ServerOptions serverOptions = new ServerOptions(clientRequest);
-                
+
+
+
+
 
             } catch (IOException e) {
                 throw new RuntimeException(e);
--- src/org/codeforall/ooptimus/ServerOptions.java
+++ src/org/codeforall/ooptimus/ServerOptions.java
@@ -1,5 +1,7 @@
 package org.codeforall.ooptimus;
 
+import java.io.File;
+
 public class ServerOptions {
 
     String clientRequest;
@@ -8,5 +10,24 @@ public class ServerOptions {
         this.clientRequest = clientRequest;
     }
 
+    File error = new File("resources/404.html");
+    File index = new File("resources/index.html");
+    File picture = new File("resources/spaceship.png");
 
+
+    String indexHeader = "HTTP/1.0 200 Document Follows\r\n" +
+            "Content-Type: text/html; charset=UTF-8\r\n" +
+            "Content-Length: " + index.length() + " \r\n" +
+            "\r\n";
+
+    String imageHeader = "HTTP / 1.0 200 Document Follows\r\n" +
+            "Content - Type:image /JPG \r\n" +
+            "Content - Length: " + picture.length() + " \r\n" +
+            "\r\n";
+
+    String errorHeader = "HTTP / 1.0 404 Not Found" +
+            "Content - Type:text / html;" +
+            "charset = UTF - 8\r\n" +
+            "Content - Length: " + error.length() + " \r\n" +
+            "\r\n";
 }
