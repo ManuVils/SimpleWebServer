a9c9bfa9f8618ba20554ac4909c079aa8cd6f02e
Emanuel Rodrigues <emanueldavid95@hotmail.com>
2024-01-23 08:17:53 +0000

    Added both switch cases that, depending on the client request, will return the apropriate header and respective file that will be sent in the server's response.

--- src/org/codeforall/ooptimus/ServerOptions.java
+++ src/org/codeforall/ooptimus/ServerOptions.java
@@ -15,19 +15,48 @@ public class ServerOptions {
     File picture = new File("resources/spaceship.png");
 
 
-    String indexHeader = "HTTP/1.0 200 Document Follows\r\n" +
-            "Content-Type: text/html; charset=UTF-8\r\n" +
-            "Content-Length: " + index.length() + " \r\n" +
-            "\r\n";
-
-    String imageHeader = "HTTP / 1.0 200 Document Follows\r\n" +
-            "Content - Type:image /JPG \r\n" +
-            "Content - Length: " + picture.length() + " \r\n" +
-            "\r\n";
-
     String errorHeader = "HTTP / 1.0 404 Not Found" +
             "Content - Type:text / html;" +
             "charset = UTF - 8\r\n" +
             "Content - Length: " + error.length() + " \r\n" +
             "\r\n";
+
+    String indexHeader = "HTTP/1.0 200 Document Follows\r\n" +
+            "Content-Type: text/html; charset=UTF-8\r\n" +
+            "Content-Length: " + index.length() + " \r\n" +
+            "\r\n";
+
+    String pictureHeader = "HTTP / 1.0 200 Document Follows\r\n" +
+            "Content - Type:image /JPG \r\n" +
+            "Content - Length: " + picture.length() + " \r\n" +
+            "\r\n";
+
+
+    public File clientRequestedFile() {
+        switch(this.clientRequest){
+            case "/spaceship.png":
+                return picture;
+
+            case "/index.html":
+                return index;
+
+            default:
+                return error;
+        }
+    }
+
+    public String fileRequestedHeader() {
+        switch(this.clientRequest){
+            case "spaceship.png":
+                return pictureHeader;
+
+            case "index.html":
+                return indexHeader;
+
+            default:
+                return errorHeader;
+        }
+    }
+
+
 }
